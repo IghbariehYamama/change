c1a6e2ad591d136122286a27ecffb85fd5b33bab
Ighbarieh Yamama <yamama@mail.tau.ac.il>
2022-08-29 10:16:07 +0300

    change

---
+++ src/components/Change.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from "react"
+import { Sale } from './Sale';
+import { Buy } from './Buy';
+
+export function Change(){
+    const [modallsOpen,SetModallsOpen]=useState(false);
+
+    function ChangeSale(){
+        SetModallsOpen(true);
+    } 
+    function ChangeBuy(){
+        SetModallsOpen(false);
+    }
+
+    return (
+        <div>
+            <button className='Sale'  onClick={ChangeSale}>Sale</button>
+            <button className='Buy' onClick={ChangeBuy}>Buy</button>
+            {modallsOpen? <Sale/>:<Buy/>}
+        </div>
+    );
+}
